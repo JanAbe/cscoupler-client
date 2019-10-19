8f1466f4a5275f697449f6b02e0c3901ac7f010b
Jan Abe <janabedewaal@protonmail.com>
2019-10-19 13:20:51 +0200

    Add experience samples to student card, and further improve styling

--- src/components/StudentCard.vue
+++ src/components/StudentCard.vue
@@ -1,10 +1,10 @@
 <template>
   <div>
-      <div class="border m-3 rounded-lg bg-white mx-auto max-w-sm shadow-md">
+      <div class="border m-1 rounded-lg bg-white mx-auto max-w-sm shadow-lg hover:shadow-none">
         <div class="sm:flex sm:items-center px-2 py-2">
           <div>
             <img class="block h-16 object-contain object-center sm:h-16 rounded-full mx-auto mb-4 sm:mb-0 sm:mr-4 sm:ml-0" src="https://api.adorable.io/avatars/196/abott@adorable.png" alt="">
-            <p class="text-sm leading-tight py-1 mr-3" 
+            <p class="text-sm py-1 mr-3" 
               v-bind:class="{available: student.status=='Available', 
                              unavailable: student.status=='Unavailable'}">
               {{student.status}}
@@ -20,9 +20,19 @@
             </div>
           </div>
         </div>
+        <div class="flex flex-wrap">
+          <p class="text-sm text-grey-dark mx-4">
+            - Interned at Fluukle company. Implemented
+            a new algorithm, decreasing cost by 20%.
+          </p>
+          <p class="text-sm text-grey-dark mx-4 py-3">
+            - Worked at Zoink.inc as junior developer.
+            Working on their main product, fixing bugs left and right.
+          </p>
+        </div>
         <div class="flex flex-wrap justify-between px-2 py-2">
-          <button class="text-xs font-semibold rounded-full px-4 py-1 bg-white border border-yellow">Message</button>
-          <button class="text-xs font-semibold rounded-full px-4 py-1 bg-white border border-tera">Curriculum Vitae</button>
+          <button class="text-xs font-semibold rounded-full px-4 py-1 bg-white border border-blue hover:bg-blue-200">Message</button>
+          <button class="text-xs font-semibold rounded-full px-4 py-1 bg-white border border-tera hover:bg-orange-300">Curriculum Vitae</button>
         </div>
       </div>
     </div>
@@ -47,12 +57,6 @@ export default {
       return student
     },
 
-    visitStudentAccount(studentID) {
-      // will go to the account page of 
-      // the student.
-      return studentID
-    },
-
     messageStudent(studentID, message) {
       // send a message to the selected
       // student
@@ -64,7 +68,8 @@ export default {
 
 <style scoped>
   .border-tera {
-    border-color: #f6dbc6;
+    /* border-color: #f6dbc6; */
+    border-color:#fbd38d;  
   }
 
   .border-blue {
