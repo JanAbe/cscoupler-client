afe9a99af3625d125b677740cf2c38d9561ce84a
Jan Abe <janabedewaal@protonmail.com>
2019-10-20 21:16:13 +0200

    Add toggle option for menu on mobile. NOTE: known issue is present
    When a user goes from a large screen to a mobile sized screen and toggles/untoggles the navbar, the menu bar gets messed up when the user goes back to the large screen. large screen -> mobile screen -> toggle menu -> large screen == messed up navbar. Also when the navbar is toggled in mobile sized mode, and the menu is thus hidden, the menu content is also hidden when the user goes back to the large screen

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,11 +4,11 @@
       <a id="home" href="#home" class="font-semibold text-xl tracking-tight">CScoupler</a>
     </div>
     <div class="block lg:hidden">
-      <button class="flex items-center px-3 py-2 border rounded text-purple-200 border-purple-400 hover:text-white hover:border-white">
+      <button @click="toggleMenuContent()" class="flex items-center px-3 py-2 border rounded text-purple-200 border-purple-400 hover:text-white hover:border-white">
         <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
       </button>
     </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+    <div id="menu-content" class="w-full flex-grow lg:flex lg:items-center lg:w-auto">
       <div class="lg:flex-grow lg:flex lg:justify-center">
         <a href="#students" class="block mt-4 lg:inline-block lg:mt-0 text-white mx-4 lg:ml-0 lg:mr-4">
           Students
@@ -20,15 +20,17 @@
           Companies
         </a>
       </div>
-      <div class="lg:flex text-white mx-4 mt-4 lg:mt-0">
-        <a href="#account">
-          Account
-        </a>
-      </div>
-      <div class="lg:flex text-white mx-4 mt-4 lg:mt-0">
-        <a href="#sign-out" class="lg:ml-4">
-          Sign out
-        </a>
+      <div class="lg:flex text-white mx-4 lg:mt-0">
+        <div class="mt-4 lg:mt-0">
+          <a href="#account">
+            Account
+          </a>
+        </div>
+        <div class="mt-4 lg:mt-0">
+          <a href="#sign-out" class="lg:ml-4">
+            Sign out
+          </a>
+        </div>
       </div>
     </div>
   </nav>
@@ -36,6 +38,16 @@
 
 <script>
 export default {
+  methods: {
+    toggleMenuContent() {
+      const menuContent = document.querySelector('#menu-content')
+      if (menuContent.style.display === 'none') {
+        menuContent.style.display = 'block'
+        return
+      }
+      menuContent.style.display = 'none'
+    }
+  }
 }
 </script>
 
