0c61f999a3cd37b64f99f1302d47edceeef14330
Jan Abe <janabedewaal@protonmail.com>
2019-10-22 00:12:30 +0200

    Implement register student functionality, a student can register himself now

--- src/components/StudentRegistration.vue
+++ src/components/StudentRegistration.vue
@@ -7,37 +7,37 @@
     <form v-on:submit.prevent="register()" id="student-form" class="lg:w-1/2 mx-auto md:ml-48 md:mr-auto max-w-lg md:mt-12 border-2 border-gray-200 rounded-lg px-2 lg:px-6 pt-8 pb-4">
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="student-first-name">
+          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="first-name">
             First Name
           </label>
-          <input id="student-first-name" type="text" placeholder="John" 
+          <input id="first-name" v-model="firstname" type="text" placeholder="John" 
             class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
         </div>
         <div class="w-full md:w-1/2 px-3">
-          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="student-last-name">
+          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="last-name">
             Last Name
           </label>
-          <input id="student-last-name" type="text" placeholder="Smith"
+          <input id="last-name" v-model="lastname" type="text" placeholder="Smith"
             class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
         </div>
       </div>
       
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
         <div class="w-full px-3 mt-3 lg:mt-0">
-          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="grid-email">
+          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="email">
             Email
           </label>
-          <input id="student-email" type="email" placeholder="johnsmith@email.com" 
+          <input id="email" v-model="email" type="email" placeholder="johnsmith@email.com" 
             class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
         </div>
       </div>
 
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
         <div class="w-full px-3 mb-3 lg:mb-1">
-          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="student-password">
+          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="password">
             Password
           </label>
-          <input id="student-password" type="password" placeholder="******************" 
+          <input id="password" v-model="password" type="password" placeholder="******************" 
             class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
           <p class="text-gray-600 leading-tight text-xs italic">It should be atleast 8 characters long, contain numbers, letters and special characters</p>
         </div>
@@ -45,25 +45,25 @@
       
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
         <div class="w-full md:w-1/2 px-3 mb-4 lg:mb-0">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="student-status">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="status">
             Student status
           </label>
-          <multi-select class="mb-3 leading-tight focus:outline-none hover:border-purple-300 focus:border-purple-300" v-model="status" :options="statusOptions" :close-on-select="true" :show-labels="false" label="name" track-by="name" placeholder="Select student status"></multi-select>
+          <multi-select class="mb-3 leading-tight focus:outline-none hover:border-purple-300 focus:border-purple-300" v-model="status" :options="statusOptions" :close-on-select="true" :show-labels="false" label="stat" track-by="stat" placeholder="Select student status"></multi-select>
         </div>
         <div class="w-full md:w-1/2 px-3">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="student-university">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="university">
             University
           </label>
-          <input id="student-university" type="text" placeholder="University of ..." 
+          <input id="university" v-model="university" type="text" placeholder="University of ..." 
             class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none hover:border-purple-300 focus:border-purple-300">
         </div>
       </div>
 
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
-        <label class="mx-3 text-gray-700 uppercase text-xs font-bold mb-2" for="student-skills">Select up to 5 of your skills</label>
+        <label class="mx-3 text-gray-700 uppercase text-xs font-bold mb-2" for="skills">Select up to 5 of your skills</label>
         <multi-select v-model="skills" :options="skillsOptions" :multiple="true" 
           :close-on-select="false" :clear-on-select="true" :max="5"
-          label="name" track-by="name" placeholder="Select your skills" class="mx-3" id="student-skills">
+          label="skill" track-by="skill" placeholder="Select your skills" class="mx-3" id="skills">
         </multi-select>
       </div>
 
@@ -102,42 +102,97 @@ export default {
   },
   data() {
     return {
+      errors: [],
+      firstname: null,
+      lastname: null,
+      email: null,
+      password: null,
+      status: null,
+      university: null,
       skills: [],
+      resume: null,
       skillsOptions: [
-        { name: 'Docker', language: 'JavaScript' },
-        { name: 'C#', language: 'JavaScript' },
-        { name: 'Java', language: 'Ruby' },
-        { name: 'Spring MVC', language: 'Ruby' },
-        { name: 'Laravel', language: 'PHP' },
-        { name: 'Go', language: 'PHP' },
-        { name: 'C++', language: 'PHP' },
-        { name: 'Unreal Engine', language: 'PHP' },
-        { name: 'Unit Testing', language: 'PHP' },
-        { name: 'PHP', language: 'Elixir' }
+        { skill: 'Docker' },
+        { skill: 'C#' },
+        { skill: 'Java' },
+        { skill: 'Spring MVC' },
+        { skill: 'Laravel' },
+        { skill: 'Go' },
+        { skill: 'C++' },
+        { skill: 'Unreal Engine' },
+        { skill: 'Unit Testing' },
+        { skill: 'PHP' }
       ],
-      status: [],
       statusOptions: [
-        { name: 'Student' },
-        { name: 'Newly-graduated' }
+        { stat: 'Student' },
+        { stat: 'Newly-graduated' }
       ]
     }
   },
   methods: {
     register() {
       const endpoint = 'http://localhost:3000/signup/student'
+      const data = {
+        user: {
+          firstname: this.firstname,
+          lastname: this.lastname,
+          email: this.email,
+          password: this.password
+        },
+        university: this.university,
+        skills: this.skills.map(({ skill }) => skill),
+        status: this.status['stat']
+      }
 
       const payload = new FormData()
-      payload.append('resume', '')
-      payload.append('studentData', JSON.stringify(''))
+      payload.append('resume', this.resume)
+      payload.append('studentData', JSON.stringify(data))
 
       axios.post(endpoint, payload)
+    },
+
+    validateForm() {
+      if (this.firstname && this.lastname &&
+          this.email && this.password && this.status.length !== 0
+          && this.skills.length !== 0 && this.resume) {
+            return true
+      }
+        
+      if (!this.firstname) {
+        this.errors.push('First name is required')
+      }
+
+      if (!this.lastname) {
+        this.errors.push('Last name is required')
+      }
+
+      if (!this.email) {
+        this.errors.push('Email is required')
+      }
+
+      if (!this.password) {
+        this.errors.push('Password is required')
+      }
+
+      if (this.skills.length === 0) {
+        this.errors.push('At least one skill needs to be selected')
+      }
+      
+      if (this.status.length === 0) {
+        this.errors.push('Status is required')
+      }
+
+      if (!this.resume) {
+        this.errors.push('Resume is required')
+      }
     },
 
     selectResume() {
       const resumeUpload = document.querySelector('#resume-upload')
       resumeUpload.addEventListener('change', (event) => {
-        const resumeName = event.target.files[0].name
-        document.querySelector('#selected-file-name').textContent = resumeName
+        const file = event.target.files[0]
+        this.resume = file
+        document.querySelector('#selected-file-name').textContent = file.name
       })
     }
   }
