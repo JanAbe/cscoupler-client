451aa3648da5348aac77649b9137b9b83ffdb7d5
Jan Abe <janabedewaal@protonmail.com>
2019-10-21 21:45:48 +0200

    Add student-status field to student registration form + improve responsiveness

--- src/components/StudentRegistration.vue
+++ src/components/StudentRegistration.vue
@@ -1,22 +1,24 @@
 <template>
-  <div class="flex h-screen">
+  <div class="flex justify-between h-screen">
     <div class="hidden lg:block lg:w-1/2 lg:bg-purple-600">
       <img src="../assets/svg/online-cv.svg" alt="" class="sm:mx-0 lg:mx-32 lg:mt-8">
     </div>
 
-    <form v-on:submit.prevent="register()" id="student-form" class="w-1/2 mx-auto max-w-lg container md:mt-12 border-2 border-gray-200 rounded-lg px-8 pt-8 pb-4">
+    <form v-on:submit.prevent="register()" id="student-form" class="w-1/2 mx-auto md:ml-48 md:mr-auto max-w-lg md:mt-12 border-2 border-gray-200 rounded-lg px-8 pt-8 pb-4">
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
         <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
+          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="student-first-name">
             First Name
           </label>
-          <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300" id="grid-first-name" type="text" placeholder="Jane">
+          <input id="student-first-name" type="text" placeholder="John" 
+            class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
         </div>
         <div class="w-full md:w-1/2 px-3">
-          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
+          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="student-last-name">
             Last Name
           </label>
-          <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300" id="grid-last-name" type="text" placeholder="Doe">
+          <input id="student-last-name" type="text" placeholder="Smith"
+            class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
         </div>
       </div>
       
@@ -25,34 +27,43 @@
           <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="grid-email">
             Email
           </label>
-          <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300" id="grid-email" type="email" placeholder="jane@email.com">
+          <input id="student-email" type="email" placeholder="johnsmith@email.com" 
+            class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
         </div>
       </div>
 
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
         <div class="w-full px-3">
-          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="grid-password">
+          <label class="block uppercase text-gray-700 text-xs font-bold mb-2" for="student-password">
             Password
           </label>
-          <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300" id="grid-password" type="password" placeholder="******************">
+          <input id="student-password" type="password" placeholder="******************" 
+            class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-1 leading-tight focus:outline-none focus:bg-white hover:border-purple-300 focus:border-purple-300">
           <p class="text-gray-600 leading-tight text-xs italic">It should be atleast 8 characters long, contain numbers, letters and special characters</p>
         </div>
       </div>
       
       <div class="flex flex-wrap mx-3 mb-6 sm:-mx-3">
-        <div class="w-full px-3">
-          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-university">
+        <div class="w-full md:w-1/2 px-3">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="student-status">
+            Student status
+          </label>
+          <multi-select class="mb-3 leading-tight focus:outline-none hover:border-purple-300 focus:border-purple-300" v-model="status" :options="statusOptions" :close-on-select="true" :show-labels="false" label="name" track-by="name" placeholder="Select student status"></multi-select>
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="student-university">
             University
           </label>
-          <input class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none hover:border-purple-300 focus:border-purple-300" id="grid-university" type="text" placeholder="University of X">
+          <input id="student-university" type="text" placeholder="University of ..." 
+            class="appearance-none block w-full text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none hover:border-purple-300 focus:border-purple-300">
         </div>
       </div>
 
       <div class="flex flex-wrap mb-6 sm:-mx-3">
-        <label class="mx-3 text-gray-700 uppercase text-xs font-bold mb-2" for="grid-skills">Select up to 5 of your skills</label>
+        <label class="mx-3 text-gray-700 uppercase text-xs font-bold mb-2" for="student-skills">Select up to 5 of your skills</label>
         <multi-select v-model="skills" :options="skillsOptions" :multiple="true" 
           :close-on-select="false" :clear-on-select="true" :max="5"
-          label="name" track-by="name" placeholder="Select your skills" class="mx-3" id="grid-skills">
+          label="name" track-by="name" placeholder="Select your skills" class="mx-3" id="student-skills">
         </multi-select>
       </div>
 
@@ -103,6 +114,11 @@ export default {
         { name: 'Unreal Engine', language: 'PHP' },
         { name: 'Unit Testing', language: 'PHP' },
         { name: 'PHP', language: 'Elixir' }
+      ],
+      status: [],
+      statusOptions: [
+        { name: 'Student' },
+        { name: 'Newly-graduated' }
       ]
     }
   },
@@ -131,6 +147,14 @@ export default {
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
 
 <style>
+  .multiselect {
+    color: #4a5568;
+  }
+
+  .multiselect__single {
+    padding-bottom: 5px;
+  }
+
   .multiselect__tag {
     border-radius: 80px;
     background: #d6bcfa;
