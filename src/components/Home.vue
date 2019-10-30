71bc61abb4642161d09e6cb93b1152e5dc24a425
Jan Abe <janabedewaal@protonmail.com>
2019-10-30 11:36:14 +0100

    Create home component

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,28 +1,55 @@
 <template>
-  <div class="container mx-auto">
+  <div class="container mx-auto bg-gray-100">
     <div class="text-center text-3xl">
       CScoupler
     </div>
-    <p class="leading-tight italic text-base text-center">
+    <p class="leading-tight italic text-base text-center mx-40">
       A platform for companies and students to 
       come in touch with one another, easing the internship hunt for students and easing the 
       quest of finding qualified students for companies.
     </p>
 
-    <div class="flex justify-center">
-      <div class="mx-2 p-4 bg-purple-500 text-white rounded-sm">
+    <div class="mt-4 mx-40">
+      <p class="text-center bg-gray-100">
+        How it works. As a student you create an account, upload your resume, write short
+        descriptions about your past experiences and tell about your skills. Then, you scroll
+        through the posted internship projects, you mark the ones your find interesting and you
+        contact a representative from the company that posted the project.
+      </p>
+      <p class="text-center mt-4 bg-gray-100">
+        As a recruiter, you browse and search to find students that you think fit
+        within your company and contact you.
+        The search goes both ways, from student to company, and from company to student.
+        CScoupler provides a central place where this can happen, with a focus on internships.
+      </p>
+    </div>
+    <div class="flex justify-center mt-4">
+      <button class="mx-2 py-2 px-2 text-sm rounded-full bg-white text-gray-700 border border-purple-400 hover:bg-purple-400 hover:text-white">
+        Register your company today
+      </button>
+
+      <button class="mx-2 py-2 px-2 text-sm rounded-full bg-white text-gray-700 border border-purple-400 hover:bg-purple-400 hover:text-white">
+        Create an account today
+      </button>
+    </div>
+
+    <div class="flex justify-center mt-4">
+      <img src="../assets/svg/online-resume.svg" class="w-2/5 my-2">
+    </div>
+
+    <div class="flex justify-center mt-4">
+      <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
         Search through over 100+ internship projects
       </div>
 
-      <div class="mx-2 p-4 bg-purple-500 text-white rounded-sm">
-        Over 70 students preceded you
+      <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
+        Over 70 registered students are looking for an internship.
       </div>
 
-      <div class="mx-2 p-4 bg-purple-500 text-white rounded-sm">
+      <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
         More than 10 companies have recruiters looking for students
       </div>
     </div>
-
   </div>
 </template>
 
