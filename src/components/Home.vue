c1b30d1cf0f6c462e9e8b35cc1d1450ee82942fd
Jan Abe <janabedewaal@protonmail.com>
2019-11-04 14:03:53 +0100

    Improve home page, by adding images of registration forms

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="container mx-auto bg-gray-100">
+  <div class="">
     <div class="text-center text-3xl">
       CScoupler
     </div>
-    <p class="leading-tight italic text-sm text-center mx-40">
+    <!-- <p class="leading-tight italic text-sm text-center mx-40">
       A platform for companies and students to 
       come in touch with one another, easing the internship hunt for students and easing the 
       quest of finding qualified students for companies.
@@ -31,13 +31,44 @@
       <router-link to="/signup/student" class="mx-2 py-2 px-2 text-sm rounded-full bg-white text-gray-700 border border-purple-400 hover:bg-purple-400 hover:text-white">
         Create an account today
       </router-link>
+    </div> -->
+
+    <div class="lg:flex lg:justify-end mt-12 lg:mx-16">
+      <div class="w-1/2 self-center pb-12 ml-8 mr-1">
+        <p class="text-gray-700">
+          Register your company and create the first representative account simultaneously.
+          Use this account to send invitations to other company-representatives.
+          <br>
+          By registering your company, you gain access to to skilled students looking for internships.
+          Browse through students, mark the ones your interested in, lookup their profile and resume,
+          and contact the ones most suitable.
+        </p>
+        <div class="flex justify-center">
+          <router-link to="/signup/company" class="mx-2 py-px px-12 text-xs rounded-full bg-purple-400 text-white border border-purple-400 hover:bg-purple-400 hover:text-white">
+            Register
+          </router-link>
+        </div>
+      </div>
+      <img id="company-form" src="../assets/company_registration.png" class="w-1/2 rounded-lg ">
+    </div>
+    
+    <div class="lg:flex lg:justify-end mt-12 lg:mx-16 mb-12">
+      <img id="student-form" height="400" src="../assets/student_registration.png" class="w-1/2 rounded-lg">
+      <div class="w-1/2 self-center pb-12 ml-8 mr-1">
+        <p class="text-gray-700">
+          Create an account and write about your skills and experiences and upload your resume.
+          Then, search for companies or internship positions / projects you are interested in!
+          Use this account to send invitations to other company-representatives.
+        </p>
+        <div class="flex justify-center">
+          <router-link to="/signup/company" class="mx-2 py-px px-12 text-xs rounded-full bg-purple-400 text-white border border-purple-400 hover:bg-purple-400 hover:text-white">
+            Register
+          </router-link>
+        </div>
+      </div>
     </div>
 
-    <div class="flex justify-center mt-4">
-      <img id="registration-form" src="../assets/company_registration.png" class="w-5/6 rounded-lg" alt="company registration form">
-    </div>
-
-    <div class="flex justify-center mt-4">
+    <!-- <div class="flex justify-center mt-4">
       <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
         Search through over 100+ internship projects
       </div>
@@ -49,7 +80,7 @@
       <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
         More than 10 companies have recruiters looking for students
       </div>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -60,7 +91,13 @@ export default {
 </script>
 
 <style scoped>
-  #registration-form {
-    box-shadow: 27.1px 62.5px 75px -25px rgba(50,50,93,.5),16.2px 37.5px 75px -37.5px rgba(0,0,0,.6);
+  #company-form {
+    box-shadow: 47.1px 42.5px 25px -25px rgba(50,50,93,.5),16.2px 37.5px 75px -37.5px rgba(0,0,0,.6);
+    transform: skewX(-2deg);
+  }
+
+  #student-form {
+    box-shadow: -47.1px 42.5px 25px -25px rgba(50,50,93,.5), 16.2px 37.5px 35px -27.5px rgba(0,0,0,.6);
+    transform: skewX(2deg);
   }
 </style>
