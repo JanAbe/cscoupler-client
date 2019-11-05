267b4e8b7ba8eaf19485e76c1ea0b4b6d011a6eb
Jan Abe <janabedewaal@protonmail.com>
2019-11-05 10:43:11 +0100

    Improve home page for ipad and phone screens

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,15 @@
 <template>
   <div>
-    <div class="text-xl pl-56 pt-8 pb-20">
+    <div class="text-xl mx-12 md:ml-24 lg:pl-56 pt-6 pb-6 md:pb-12 lg:pb-20">
       CScoupler, the new <u>platform</u> for finding computer-science related <u>internships</u>.
       <br>
       Find your internships, or <u>suitable candidates</u> here.
     </div>
 
     <div class="lg:flex lg:mx-16 pb-10 border-b border-purple-400">
-      <div class="w-1/2 ml-8 mr-1">
-        <h2 class="px-32 text-center text-gray-700 text-2xl pb-8">Companies</h2>
-        <p class="text-gray-700 px-32 tracking-wide">
+      <div class="lg:w-1/2 ml-8 mr-1">
+        <h2 class="md:px-32 text-center text-gray-700 text-2xl pb-2 md:pb-5 lg:pb-8">Companies</h2>
+        <p class="text-gray-700 md:px-16 lg:px-32 tracking-wide">
           Register your company and create the first representative account simultaneously.
           <br>
           Use this account to send invitations to other company-representatives.
@@ -18,20 +18,20 @@
           Browse through students, mark the ones you are interested in, look up their profile and resume,
           and contact the ones most suitable.
         </p>
-        <div class="flex justify-center my-8">
+        <div class="flex justify-center mt-8 lg:my-8">
           <router-link to="/signup/company" class="uppercase rounded mx-2 py-3 px-12 text-sm font-semibold bg-purple-400 text-white border border-purple-400 hover:bg-purple-500 hover:text-white">
             Sign up for free
           </router-link>
         </div>
       </div>
-      <img id="company-form" src="../assets/company_registration.png" class="w-1/2 rounded-lg ">
+      <img id="company-form" src="../assets/company_registration.png" class="hidden lg:block lg:w-1/2 lg:rounded-lg ">
     </div>
     
-    <div class="lg:flex lg:items-start mt-12 lg:mx-16 mb-12">
-      <img id="student-form" height="400" src="../assets/student_registration.png" class="w-1/2 rounded-lg">
-      <div class="w-1/2 ml-8 mr-1 mt-24">
-        <h2 class="px-32 text-center text-gray-700 text-2xl pb-8">Students</h2>
-        <p class="text-gray-700 px-32">
+    <div class="lg:flex lg:items-start md:mt-12 lg:mt-10 lg:mx-16 mb-12">
+      <img id="student-form" height="400" src="../assets/student_registration.png" class="hidden lg:block lg:w-1/2 lg:rounded-lg">
+      <div class="lg:w-1/2 ml-8 mr-1 lg:mt-24">
+        <h2 class="md:px-32 text-center text-gray-700 text-2xl pb-2 md:pb-5 lg:pb-8">Students</h2>
+        <p class="text-gray-700 md:px-16 lg:px-32">
           Create an account, write about your skills and experiences and upload your resume.
           Then, search for companies or internship positions / projects you are interested in!
         </p>
@@ -42,20 +42,6 @@
         </div>
       </div>
     </div>
-
-    <!-- <div class="flex justify-center mt-4">
-      <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
-        Search through over 100+ internship projects
-      </div>
-
-      <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
-        Over 70 registered students are looking for an internship.
-      </div>
-
-      <div class="mx-2 py-4 px-2 bg-purple-500 text-white rounded-sm">
-        More than 10 companies have recruiters looking for students
-      </div>
-    </div> -->
   </div>
 </template>
 
